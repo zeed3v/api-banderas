ae6fb928bb3565fb15a49bb2bb193810f96f3296
zeed3v <felipebrionese@gmail.com>
2022-04-17 02:35:57 -0400

    commit

---
+++ src/components/Card.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="card">
+      <div class="card-body">
+          <h5>{{pais.name}}</h5>
+          <p class="card-text">
+              <span class="badge badge-dark d-block mb-1">nativeName: {{pais.nativeName}}</span>
+              <span class="badge badge-info p-3 d-block mb-1">population: {{pais.population}}</span>
+              <span class="badge badge-dark d-block mb-1">capital: {{pais.capital}}</span>
+              <span class="badge badge-dark d-block mb-1">region: {{pais.region}}</span>
+          </p>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['pais']
+
+}
+</script>
+
--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,38 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    paises: [],
+    paisesFiltrados:[]
   },
   getters: {
   },
   mutations: {
+    setPaises (state, payload) {
+      state.paises = payload
+    },
+    setPaisesFiltrados(state, payload) {
+      state.PaisesFiltrados = payload
+    }
   },
   actions: {
+    async getPaises ({ commit }){
+      try {
+        const res = await fetch('https://restcountries.com/v2/all')
+        const data = await res.json()
+        //console.log(data)
+        commit('setPaises', data)
+      } catch (error) {
+        console.log(error)
+        
+      }
+    }
+  },
+  getters: {
+    topPaisesPoblacion(state) {
+      return state.paises.sort((a, b) => {
+        return a.population < b.population ? 1 : -1
+      })
+    }
   },
   modules: {
   }
